2fc0ea73b76016c5af6823f4b310aee1d127a5b0
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-04-27 21:58:09 +0430

    new message added

--- src/New.jsx
+++ src/New.jsx
@@ -1,9 +1,10 @@
 export default function New(){
     return (
-        <div>
-            <h1>
+        <div className="w-full h-fit  flex justify-center items-center ">
+            <h1 className="font-bold font-serif text-3xl text-red-800  ">
                 This message comes from the New component.
             </h1>
+            <p className=" font-bold font-serif text-3xl text-red-800 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quidem dolore saepe voluptates labore maxime quod suscipit consequuntur consequatur laudantium magnam eum natus, sunt porro beatae obcaecati culpa provident dignissimos?</p>
         </div>
     )
 }
